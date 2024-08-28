7de18fb5aeb1caf0cb5c2b91c4f6d20d542bf2e0
jonkadelic <josephdotbrown@gmail.com>
2024-08-28 22:35:55 +0100

    moved to Meson for build system

--- src/main.c
+++ src/main.c
@@ -1,13 +1,17 @@
-#include "rudyscung.h"
-#include "world/tile.h"
+#include <stdio.h>
 
 #include <SDL2/SDL.h>
+
+#include "rudyscung.h"
+#include "world/tile.h"
 
 int main(int argc, char** argv) {
     char const* resources_path = "./res";
     if (argc > 1) {
         resources_path = argv[1];
     }
+
+    printf("Starting RudyScung...\n");
 
     // Static initialization
     tiles_init();
